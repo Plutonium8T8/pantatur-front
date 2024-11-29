d649d5ac75787d455375287e919926e27d08ac78
maksimbordan <bordan.maxim@gmail.com>
2024-11-29 12:47:54 +0200

    fix user id global

--- src/UserContext.js
+++ src/UserContext.js
@@ -1,11 +1,25 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
 // Создаем контекст для хранения user_id
 const UserContext = createContext();
 
 // Провайдер для контекста
 export const UserProvider = ({ children }) => {
-  const [userId, setUserId] = useState(null);
+  // Чтение user_id из localStorage при инициализации состояния
+  const [userId, setUserId] = useState(() => {
+    const savedUserId = localStorage.getItem('user_id');
+    return savedUserId ? savedUserId : null;
+  });
+
+  // Запись user_id в localStorage при его изменении
+  useEffect(() => {
+    if (userId) {
+      localStorage.setItem('user_id', userId);
+    } else {
+      localStorage.removeItem('user_id'); // Если userId очищается, удаляем его из localStorage
+    }
+  }, [userId]);
+
   return (
     <UserContext.Provider value={{ userId, setUserId }}>
       {children}
