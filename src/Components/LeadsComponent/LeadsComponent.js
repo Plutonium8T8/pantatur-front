b4c67a4263acccb71f81049bfb8e5d9b5b3fca36
Rusu Cristian <39808975+Plutonium8T8@users.noreply.github.com>
2025-01-20 13:35:16 +0200

    Revert "Merge branch 'dev-crm-3' into Restyle-3"
    This reverts commit 52ab5510b953f41606132a4d778057868003597b, reversing
    changes made to cab5a2b6910264a39993eca2f45ad5d80cc13293.

--- src/Components/LeadsComponent/LeadsComponent.js
+++ src/Components/LeadsComponent/LeadsComponent.js
@@ -16,78 +16,78 @@ import '../SnackBarComponent/SnackBarComponent.css'
 import { FaCircleNotch, FaEnvelope, FaTrash, FaXbox } from 'react-icons/fa';
 
 export const updateTicket = async (updateData) => {
-  try {
-    const token = Cookies.get('jwt');
+    try {
+        const token = Cookies.get('jwt');
 
-    if (!token) {
-      throw new Error('Token is missing. Authorization required.');
+        if (!token) {
+            throw new Error('Token is missing. Authorization required.');
+        }
+
+        const response = await fetch(`https://pandatur-api.com/tickets/${updateData.id}`, {
+            method: 'PATCH',
+            headers: {
+                Authorization: `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+            body: JSON.stringify(updateData),
+        });
+
+        if (!response.ok) {
+            const errorDetails = await response.json();
+            throw new Error(
+                `Error updating ticket: ${response.status} ${response.statusText}. Details: ${JSON.stringify(errorDetails)}`
+            );
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error('Error updating ticket:', error.message || error);
+        throw error;
     }
-
-    const response = await fetch(`https://pandatur-api.com/tickets/${updateData.id}`, {
-      method: 'PATCH',
-      headers: {
-        Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json',
-      },
-      credentials: 'include',
-      body: JSON.stringify(updateData),
-    });
-
-    if (!response.ok) {
-      const errorDetails = await response.json();
-      throw new Error(
-        `Error updating ticket: ${response.status} ${response.statusText}. Details: ${JSON.stringify(errorDetails)}`
-      );
-    }
-
-    return await response.json();
-  } catch (error) {
-    console.error('Error updating ticket:', error.message || error);
-    throw error;
-  }
 };
 
 const Leads = (selectClientId) => {
-  const [tickets, setTickets] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [currentTicket, setCurrentTicket] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [contextMenu, setContextMenu] = useState(null);
-  const contextMenuRef = useRef(null);
-  const socket = useSocket();
-  const { userId } = useUser();
-  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+    const [tickets, setTickets] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [currentTicket, setCurrentTicket] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [contextMenu, setContextMenu] = useState(null);
+    const contextMenuRef = useRef(null);
+    const socket = useSocket();
+    const { userId } = useUser();
+    const { enqueueSnackbar, closeSnackbar } = useSnackbar();
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const fetchTickets = async () => {
-    setIsLoading(true);
-    try {
-      const token = Cookies.get('jwt');
-      const response = await fetch('https://pandatur-api.com/tickets', {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json',
-        },
-        credentials: 'include',
-      });
+    const fetchTickets = async () => {
+        setIsLoading(true);
+        try {
+            const token = Cookies.get('jwt');
+            const response = await fetch('https://pandatur-api.com/tickets', {
+                method: 'GET',
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    'Content-Type': 'application/json',
+                },
+                credentials: 'include',
+            });
 
-      if (!response.ok) throw new Error('Failed to fetch tickets.');
+            if (!response.ok) throw new Error('Failed to fetch tickets.');
 
-      const data = await response.json();
-      setTickets(data);
-    } catch (error) {
-      console.error('Error fetching tickets:', error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+            const data = await response.json();
+            setTickets(data);
+        } catch (error) {
+            console.error('Error fetching tickets:', error);
+        } finally {
+            setIsLoading(false);
+        }
+    };
 
-  useEffect(() => {
-    fetchTickets();
-  }, []);
+    useEffect(() => {
+        fetchTickets();
+    }, []);
 
     const updateTicketWorkflow = async (clientId, newWorkflow) => {
         // Update state locally for immediate UI feedback
@@ -105,41 +105,41 @@ const Leads = (selectClientId) => {
     fetchTickets();
   };
 
-  const openCreateTicketModal = () => {
-    setCurrentTicket({
-      contact: '',
-      transport: '',
-      country: '',
-      priority: priorityOptions[0],
-      workflow: workflowOptions[0],
-      service_reference: '',
-      technician_id: 0,
-    });
-    setIsModalOpen(true);
-  };
+    const openCreateTicketModal = () => {
+        setCurrentTicket({
+            contact: '',
+            transport: '',
+            country: '',
+            priority: priorityOptions[0],
+            workflow: workflowOptions[0],
+            service_reference: '',
+            technician_id: 0,
+        });
+        setIsModalOpen(true);
+    };
 
-  const closeModal = () => {
-    setCurrentTicket(null);
-    setIsModalOpen(false);
-  };
+    const closeModal = () => {
+        setCurrentTicket(null);
+        setIsModalOpen(false);
+    };
 
-  const handleContextMenu = (event, ticket) => {
-    event.preventDefault();
-    setContextMenu({
-      mouseX: event.clientX - 2,
-      mouseY: event.clientY - 4,
-      ticket,
-    });
-  };
+    const handleContextMenu = (event, ticket) => {
+        event.preventDefault();
+        setContextMenu({
+            mouseX: event.clientX - 2,
+            mouseY: event.clientY - 4,
+            ticket,
+        });
+    };
 
-  const handleCloseContextMenu = () => setContextMenu(null);
+    const handleCloseContextMenu = () => setContextMenu(null);
 
-  useEffect(() => {
-    if (socket) {
-      const receiveMessage = (event) => {
-        try {
-          const message = JSON.parse(event.data);
-          console.log('Parsed WebSocket message notifications:', message);
+    useEffect(() => {
+        if (socket) {
+            const receiveMessage = (event) => {
+                try {
+                    const message = JSON.parse(event.data);
+                    console.log('Parsed WebSocket message notifications:', message);
 
                     switch (message.type) {
                         case 'message':
@@ -225,70 +225,69 @@ const Leads = (selectClientId) => {
                 }
             };
 
-      // Устанавливаем обработчики WebSocket
-      socket.onopen = () => console.log('WebSocket подключен');
-      socket.onerror = (error) => console.error('WebSocket ошибка:', error);
-      socket.onclose = () => console.log('WebSocket закрыт');
-      socket.addEventListener('message', receiveMessage);
+            // Устанавливаем обработчики WebSocket
+            socket.onopen = () => console.log('WebSocket подключен');
+            socket.onerror = (error) => console.error('WebSocket ошибка:', error);
+            socket.onclose = () => console.log('WebSocket закрыт');
+            socket.addEventListener('message', receiveMessage);
 
-      // Очистка обработчиков при размонтировании
-      return () => {
-        socket.removeEventListener('message', receiveMessage);
-        socket.onopen = null;
-        socket.onerror = null;
-        socket.onclose = null;
-      };
-    }
-  }, [socket, enqueueSnackbar, userId]);
+            // Очистка обработчиков при размонтировании
+            return () => {
+                socket.removeEventListener('message', receiveMessage);
+                socket.onopen = null;
+                socket.onerror = null;
+                socket.onclose = null;
+            };
+        }
+    }, [socket, selectClientId, enqueueSnackbar, userId]);
 
-  return (
-    <div className="dashboard-container">
-      <div className="dashboard-header">
-        <div className="header">
-          <button onClick={openCreateTicketModal} className="button-add-ticket">
-            Add Ticket
-          </button>
-          <input
-            type="text"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            placeholder="Search tickets..."
-            className="search-input"
-          />
+    return (
+        <div className="dashboard-container">
+            <div className="dashboard-header">
+                <div className="header">
+                    <button onClick={openCreateTicketModal} className="button-add-ticket">
+                        Add Ticket
+                    </button>
+                    <input
+                        type="text"
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                        placeholder="Search tickets..."
+                        className="search-input"
+                    />
+                </div>
+            </div>
+            <div className="container-tickets">
+                {workflowOptions.map((workflow) => (
+                    <WorkflowColumn
+                        key={workflow}
+                        workflow={workflow}
+                        tickets={tickets}
+                        searchTerm={searchTerm}
+                        onUpdateWorkflow={updateTicketWorkflow}
+                        onEditTicket={setCurrentTicket}
+                        onContextMenu={handleContextMenu}
+                    />
+                ))}
+            </div>
+            {isLoading && <SpinnerOverlay />}
+            {contextMenu && (
+                <ContextMenu
+                    contextMenu={contextMenu}
+                    onClose={handleCloseContextMenu}
+                    onEditTicket={setCurrentTicket}
+                    ref={contextMenuRef}
+                />
+            )}
+            {currentTicket && (
+                <TicketModal
+                    ticket={currentTicket}
+                    onClose={closeModal}
+                    onSave={fetchTickets} // Reload tickets after saving
+                />
+            )}
         </div>
-      </div>
-      <div className="container-tickets">
-        {workflowOptions.map((workflow) => (
-          <WorkflowColumn
-            key={workflow}
-            workflow={workflow}
-            tickets={tickets}
-            searchTerm={searchTerm}
-            onUpdateWorkflow={updateTicketWorkflow}
-            onEditTicket={setCurrentTicket}
-            onContextMenu={handleContextMenu}
-          />
-        ))}
-      </div>
-      {isLoading && <SpinnerOverlay />}
-      {contextMenu && (
-        <ContextMenu
-          contextMenu={contextMenu}
-          onClose={handleCloseContextMenu}
-          onEditTicket={setCurrentTicket}
-          ref={contextMenuRef}
-        />
-      )}
-      {currentTicket && (
-        <TicketModal
-          ticket={currentTicket}
-          onClose={closeModal}
-          onSave={fetchTickets} // Reload tickets after saving
-          onDelete={fetchTickets} // Для обновления после удаления
-        />
-      )}
-    </div>
-  );
+    );
 };
 
 export default Leads;
