35a6410486043d05d5cf00554996025b79dfa992
maksimbordan <bordan.maxim@gmail.com>
2025-01-21 16:14:25 +0200

    fix roles admin panel

--- src/Components/AdminPanelComponent/ModalWithToggles.js
+++ src/Components/AdminPanelComponent/ModalWithToggles.js
@@ -1,43 +1,69 @@
-import React, { useState } from "react";
-import Cookies from 'js-cookie';
+import React, { useEffect, useState } from "react";
+import Cookies from "js-cookie";
 import "./ModalWithToggles.css";
 
 const ModalWithToggles = ({ employee, closeModal }) => {
-    // Состояния для Dashboard
     const [dashboardRead, setDashboardRead] = useState(false);
     const [dashboardEdit, setDashboardEdit] = useState(false);
     const [dashboardAdmin, setDashboardAdmin] = useState(false);
 
-    // Состояния для Lead
     const [leadRead, setLeadRead] = useState(false);
     const [leadEdit, setLeadEdit] = useState(false);
     const [leadAdmin, setLeadAdmin] = useState(false);
 
-    // Состояния для Chat
     const [chatRead, setChatRead] = useState(false);
     const [chatEdit, setChatEdit] = useState(false);
     const [chatAdmin, setChatAdmin] = useState(false);
 
-    // Функция для сохранения всех разрешений на сервер
-    const saveAllPermissions = async () => {
-        const role = {
-            DASHBOARD_READ: dashboardRead,
-            DASHBOARD_WRITE: dashboardEdit,
-            DASHBOARD_ADMIN: dashboardAdmin,
-            LEAD_READ: leadRead,
-            LEAD_WRITE: leadEdit,
-            LEAD_ADMIN: leadAdmin,
-            CHAT_READ: chatRead,
-            CHAT_WRITE: chatEdit,
-            CHAT_ADMIN: chatAdmin,
-        };
+    const getSession = async () => {
+        const token = Cookies.get("jwt");
+        try {
+            const response = await fetch('https://pandatur-api.com/session', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                },
+                credentials: 'include',
+            });
 
-        console.log("Подготовка данных для отправки на сервер:");
-        console.log("Technician ID:", employee.id);
-        console.log("Permissions:", role);
+            if (!response.ok) {
+                throw new Error(`Ошибка: ${response.status}`);
+            }
 
+            const sessionData = await response.json();
+            console.log("Сессия успешно получена:", sessionData);
+
+            // Проверяем, совпадает ли ID из сессии с ID сотрудника
+            if (sessionData.user_id === employee.id) {
+                const roles = sessionData.roles || [];
+                setDashboardRead(roles.includes("DASHBOARD_READ"));
+                setDashboardEdit(roles.includes("DASHBOARD_WRITE"));
+                setDashboardAdmin(roles.includes("DASHBOARD_ADMIN"));
+
+                setLeadRead(roles.includes("LEAD_READ"));
+                setLeadEdit(roles.includes("LEAD_WRITE"));
+                setLeadAdmin(roles.includes("LEAD_ADMIN"));
+
+                setChatRead(roles.includes("CHAT_READ"));
+                setChatEdit(roles.includes("CHAT_WRITE"));
+                setChatAdmin(roles.includes("CHAT_ADMIN"));
+            } else {
+                console.log(`ID из сессии (${sessionData.user_id}) не совпадает с ID сотрудника (${employee.id}).`);
+            }
+        } catch (error) {
+            console.error("Ошибка при получении сессии:", error);
+        }
+    };
+
+    useEffect(() => {
+        getSession();
+    }, []);
+
+    const sendPermissionToServer = async (role) => {
         try {
             const token = Cookies.get("jwt");
+            console.log(`Отправка: role=${role}`);
 
             const response = await fetch("https://pandatur-api.com/admin/users/roles", {
                 method: "POST",
@@ -46,8 +72,8 @@ const ModalWithToggles = ({ employee, closeModal }) => {
                     Authorization: `Bearer ${token}`,
                 },
                 body: JSON.stringify({
-                    id: employee.id, // ID сотрудника
-                    role, // Все разрешения
+                    id: employee.id,
+                    role,
                 }),
             });
 
@@ -55,10 +81,16 @@ const ModalWithToggles = ({ employee, closeModal }) => {
                 throw new Error(`Ошибка: ${response.status}`);
             }
 
-            console.log("Все разрешения успешно сохранены:", role);
+            console.log(`Разрешение "${role}" успешно обновлено.`);
         } catch (error) {
-            console.error("Ошибка при сохранении разрешений на сервер:", error);
+            console.error(`Ошибка при обновлении разрешения "${role}":`, error);
         }
+    };
+
+    const handleToggleChange = (setter, permission) => {
+        const newValue = !setter(false);
+        setter(newValue);
+        sendPermissionToServer(permission);
     };
 
     return (
@@ -83,7 +115,7 @@ const ModalWithToggles = ({ employee, closeModal }) => {
                                     <input
                                         type="checkbox"
                                         checked={dashboardRead}
-                                        onChange={() => setDashboardRead(!dashboardRead)}
+                                        onChange={() => handleToggleChange(setDashboardRead, "DASHBOARD_READ")}
                                     />
                                     <span className="slider"></span>
                                 </label>
@@ -94,8 +126,7 @@ const ModalWithToggles = ({ employee, closeModal }) => {
                                     <input
                                         type="checkbox"
                                         checked={dashboardEdit}
-                                        onChange={() => setDashboardEdit(!dashboardEdit)}
-                                        disabled
+                                        onChange={() => handleToggleChange(setDashboardEdit, "DASHBOARD_WRITE")}
                                     />
                                     <span className="slider"></span>
                                 </label>
@@ -106,8 +137,7 @@ const ModalWithToggles = ({ employee, closeModal }) => {
                                     <input
                                         type="checkbox"
                                         checked={dashboardAdmin}
-                                        onChange={() => setDashboardAdmin(!dashboardAdmin)}
-                                        disabled
+                                        onChange={() => handleToggleChange(setDashboardAdmin, "DASHBOARD_ADMIN")}
                                     />
                                     <span className="slider"></span>
                                 </label>
@@ -121,7 +151,7 @@ const ModalWithToggles = ({ employee, closeModal }) => {
                                     <input
                                         type="checkbox"
                                         checked={leadRead}
-                                        onChange={() => setLeadRead(!leadRead)}
+                                        onChange={() => handleToggleChange(setLeadRead, "LEAD_READ")}
                                     />
                                     <span className="slider"></span>
                                 </label>
@@ -132,7 +162,7 @@ const ModalWithToggles = ({ employee, closeModal }) => {
                                     <input
                                         type="checkbox"
                                         checked={leadEdit}
-                                        onChange={() => setLeadEdit(!leadEdit)}
+                                        onChange={() => handleToggleChange(setLeadEdit, "LEAD_WRITE")}
                                     />
                                     <span className="slider"></span>
                                 </label>
@@ -143,7 +173,7 @@ const ModalWithToggles = ({ employee, closeModal }) => {
                                     <input
                                         type="checkbox"
                                         checked={leadAdmin}
-                                        onChange={() => setLeadAdmin(!leadAdmin)}
+                                        onChange={() => handleToggleChange(setLeadAdmin, "LEAD_ADMIN")}
                                     />
                                     <span className="slider"></span>
                                 </label>
@@ -157,7 +187,7 @@ const ModalWithToggles = ({ employee, closeModal }) => {
                                     <input
                                         type="checkbox"
                                         checked={chatRead}
-                                        onChange={() => setChatRead(!chatRead)}
+                                        onChange={() => handleToggleChange(setChatRead, "CHAT_READ")}
                                     />
                                     <span className="slider"></span>
                                 </label>
@@ -168,7 +198,7 @@ const ModalWithToggles = ({ employee, closeModal }) => {
                                     <input
                                         type="checkbox"
                                         checked={chatEdit}
-                                        onChange={() => setChatEdit(!chatEdit)}
+                                        onChange={() => handleToggleChange(setChatEdit, "CHAT_WRITE")}
                                     />
                                     <span className="slider"></span>
                                 </label>
@@ -179,7 +209,7 @@ const ModalWithToggles = ({ employee, closeModal }) => {
                                     <input
                                         type="checkbox"
                                         checked={chatAdmin}
-                                        onChange={() => setChatAdmin(!chatAdmin)}
+                                        onChange={() => handleToggleChange(setChatAdmin, "CHAT_ADMIN")}
                                     />
                                     <span className="slider"></span>
                                 </label>
@@ -190,16 +220,6 @@ const ModalWithToggles = ({ employee, closeModal }) => {
                 <div className="modal-footer">
                     <button className="close-button-toggle" onClick={closeModal}>
                         Cancel
-                    </button>
-
-                    <button
-                        className="save-button"
-                        onClick={async () => {
-                            await saveAllPermissions(); // Сохранение всех изменений
-                            closeModal(); // Закрытие модального окна
-                        }}
-                    >
-                        Save
                     </button>
                 </div>
             </div>
