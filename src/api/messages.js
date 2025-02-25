89298fd088dceb934559a0fd431d4a725d517042
DanNuta <dany.condurari@gmai.com>
2025-02-25 16:20:35 +0200

    feat: migrate fetch to axios for session endpoint

--- src/api/messages.js
+++ src/api/messages.js
@@ -2,13 +2,13 @@ import { baseAxios } from "./baseAxios"
 
 export const messages = {
     list: async () => { 
-        const {data } = await baseAxios.get("/tickets")
+        const {data } = await baseAxios.get("/api/tickets")
 
         return data
     },
 
     messagesTicketById: async (id) => {
-        const { data } = await baseAxios.get(`/messages/ticket/${id}`)
+        const { data } = await baseAxios.get(`/api/messages/ticket/${id}`)
 
         return data
     }
--- src/api/tickets.js
+++ src/api/tickets.js
@@ -2,19 +2,19 @@ import { baseAxios } from "./baseAxios"
 
 export const tickets = {
     list: async () => { 
-        const {data } = await baseAxios.get("/tickets")
+        const {data } = await baseAxios.get("/api/tickets")
 
         return data
     },
 
     getById: async (id) => {
-        const {data } = await baseAxios.get(`/tickets/${id}`)
+        const {data } = await baseAxios.get(`/api/tickets/${id}`)
 
         return data
     },
 
     updateById: async (id, body) => {
-        const {data} = await baseAxios.patch(`/tickets/${id}`, body)
+        const {data} = await baseAxios.patch(`/api/tickets/${id}`, body)
 
         return data
     }
