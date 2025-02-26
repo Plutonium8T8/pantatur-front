f9b368b944b86cc6635ee7254a7d660859f24fa7
DanNuta <dany.condurari@gmai.com>
2025-02-27 01:06:01 +0200

    refactor: delete origin from axios header

--- src/api/baseAxios.js
+++ src/api/baseAxios.js
@@ -6,7 +6,6 @@ export const baseAxios = axios.create({
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
-    // Origin: process.env.REACT_APP_ORIGIN_HEADER,
   },
 });
 
