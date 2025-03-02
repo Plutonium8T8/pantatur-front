d9314af91de395a5c97128b6677f5456f26743e6
DanNuta <88950510+DanNuta@users.noreply.github.com>
2025-03-02 20:47:04 +0200

    fix: leads filter (#16)
    * fix: leads filter
    
    * refactor(useDOMElementHeight): add newline at end of file
    
    * refactor(useDOMElementHeight): rename param to refDomElement
    
    * refactor: Check if refDomElement.current is truthy before updating the state
    
    * refactor(useDOMElementHeight): delete Optional chaining
    
    ---------
    
    Co-authored-by: DanNuta <dany.condurari@gmai.com>

--- src/Components/LeadsComponent/LeadsComponent.js
+++ src/Components/LeadsComponent/LeadsComponent.js
@@ -1,38 +1,45 @@
-import React, { useState, useMemo, useEffect } from 'react';
-import { useAppContext } from '../../AppContext';
-import { priorityOptions } from '../../FormOptions/PriorityOption';
-import { workflowOptions } from '../../FormOptions/WorkFlowOption';
-import WorkflowColumn from './WorkflowColumnComponent';
-import TicketModal from './TicketModal/TicketModalComponent';
-import TicketFilterModal from './TicketFilterModal';
-import TicketRow from './TicketRowComponent';
-import '../../App.css';
-import '../SnackBarComponent/SnackBarComponent.css';
-import { FaFilter, FaTable, FaColumns, FaTrash, FaEdit } from 'react-icons/fa';
-import { getLanguageByKey } from '../../Components/utils/getTranslationByKey'
-import { SpinnerRightBottom } from "../SpinnerRightBottom"
+import React, { useState, useMemo, useEffect, useRef, useLayoutEffect } from "react";
+import { useAppContext } from "../../AppContext";
+import { priorityOptions } from "../../FormOptions/PriorityOption";
+import { workflowOptions } from "../../FormOptions/WorkFlowOption";
+import WorkflowColumn from "./WorkflowColumnComponent";
+import TicketModal from "./TicketModal/TicketModalComponent";
+import TicketFilterModal from "./TicketFilterModal";
+import TicketRow from "./TicketRowComponent";
+import "../../App.css";
+import "../SnackBarComponent/SnackBarComponent.css";
+import { FaFilter, FaTable, FaColumns, FaTrash, FaEdit } from "react-icons/fa";
+import { getLanguageByKey } from "../../Components/utils/getTranslationByKey";
+import { SpinnerRightBottom } from "../SpinnerRightBottom";
+import { useDOMElementHeight } from "../../hooks"
 
 const Leads = () => {
+  const refLeadsFilter = useRef();
+
   const { tickets, isLoading, setTickets } = useAppContext();
   const [isTableView, setIsTableView] = useState(false);
   const [filteredTicketIds, setFilteredTicketIds] = useState(null);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [currentTicket, setCurrentTicket] = useState(null);
   const [isFilterOpen, setIsFilterOpen] = useState(false);
   const [selectedTickets, setSelectedTickets] = useState([]);
   const [selectedWorkflow, setSelectedWorkflow] = useState(
-    workflowOptions.filter(wf => wf !== "Realizat cu succes" && wf !== "Închis și nerealizat")
+    workflowOptions.filter(
+      (wf) => wf !== "Realizat cu succes" && wf !== "Închis și nerealizat",
+    ),
   );
+  const leadsFilterHeight = useDOMElementHeight(refLeadsFilter)
+
 
   const [filters, setFilters] = useState({
-    creation_date: '',
-    last_interaction_date: '',
+    creation_date: "",
+    last_interaction_date: "",
     technician_id: [],
-    sender_id: '',
+    sender_id: "",
     workflow: selectedWorkflow,
     priority: [],
-    tags: '',
+    tags: "",
     platform: [],
   });
 
@@ -41,9 +48,11 @@ const Leads = () => {
     let result = tickets;
     if (filteredTicketIds === null) return result;
     if (filteredTicketIds.length === 0) return [];
-    result = result.filter(ticket => filteredTicketIds.includes(ticket.id));
+    result = result.filter((ticket) => filteredTicketIds.includes(ticket.id));
     if (selectedWorkflow.length > 0) {
-      result = result.filter(ticket => selectedWorkflow.includes(ticket.workflow));
+      result = result.filter((ticket) =>
+        selectedWorkflow.includes(ticket.workflow),
+      );
     }
     return result;
   }, [tickets, filteredTicketIds, selectedWorkflow]);
@@ -51,14 +60,16 @@ const Leads = () => {
   // Выбор тикетов
   const toggleSelectTicket = (ticketId) => {
     setSelectedTickets((prev) =>
-      prev.includes(ticketId) ? prev.filter((id) => id !== ticketId) : [...prev, ticketId]
+      prev.includes(ticketId)
+        ? prev.filter((id) => id !== ticketId)
+        : [...prev, ticketId],
     );
   };
 
   // Выбор всех тикетов
   const toggleSelectAll = (e) => {
     if (e.target.checked) {
-      setSelectedTickets(filteredTickets.map(ticket => ticket.id));
+      setSelectedTickets(filteredTickets.map((ticket) => ticket.id));
     } else {
       setSelectedTickets([]);
     }
@@ -67,7 +78,9 @@ const Leads = () => {
   // Массовое удаление тикетов
   const deleteSelectedTickets = () => {
     if (selectedTickets.length === 0) return;
-    const newTickets = tickets.filter(ticket => !selectedTickets.includes(ticket.id));
+    const newTickets = tickets.filter(
+      (ticket) => !selectedTickets.includes(ticket.id),
+    );
     setTickets(newTickets);
     setSelectedTickets([]);
   };
@@ -76,7 +89,9 @@ const Leads = () => {
     if (selectedTickets.length === 0) return;
 
     // Открываем модалку редактирования с первым выделенным тикетом
-    const ticketToEdit = tickets.find(ticket => ticket.id === selectedTickets[0]);
+    const ticketToEdit = tickets.find(
+      (ticket) => ticket.id === selectedTickets[0],
+    );
     if (ticketToEdit) {
       setCurrentTicket(ticketToEdit);
       setIsModalOpen(true);
@@ -85,12 +100,12 @@ const Leads = () => {
 
   const openCreateTicketModal = () => {
     setCurrentTicket({
-      contact: '',
-      transport: '',
-      country: '',
+      contact: "",
+      transport: "",
+      country: "",
       priority: priorityOptions[0],
       workflow: workflowOptions[0],
-      service_reference: '',
+      service_reference: "",
       technician_id: 0,
     });
     setIsModalOpen(true);
@@ -107,8 +122,8 @@ const Leads = () => {
   }, [tickets, filteredTicketIds]);
 
   return (
-    <div className="dashboard-container">
-      <div className="dashboard-header">
+    <>
+      <div ref={refLeadsFilter} className="dashboard-header">
         <div className="header">
           <button onClick={openCreateTicketModal} className="button-add-ticket">
             {getLanguageByKey("Adaugă lead")}
@@ -121,123 +136,155 @@ const Leads = () => {
             placeholder={getLanguageByKey("Cauta dupa Lead, Client sau Tag")}
             className="search-input"
           />
-          <button onClick={() => setIsTableView(prev => !prev)} className="button-toggle-view">
+          <button
+            onClick={() => setIsTableView((prev) => !prev)}
+            className="button-toggle-view"
+          >
             {isTableView ? <FaColumns /> : <FaTable />}
-             {getLanguageByKey(isTableView ? "Coloană" : "Listă")}
+            {getLanguageByKey(isTableView ? "Coloană" : "Listă")}
           </button>
 
           <div className="ticket-counter-row">
-            {getLanguageByKey("Toate tichetele")}: {tickets.length} | {getLanguageByKey("Filtrate")}: {filteredTickets.length}
+            {getLanguageByKey("Toate tichetele")}: {tickets.length} |{" "}
+            {getLanguageByKey("Filtrate")}: {filteredTickets.length}
           </div>
 
           {selectedTickets.length > 0 && (
-            <button onClick={deleteSelectedTickets} className="button-delete-row">
-              <FaTrash /> {getLanguageByKey("Ștergere")} ({selectedTickets.length})
+            <button
+              onClick={deleteSelectedTickets}
+              className="button-delete-row"
+            >
+              <FaTrash /> {getLanguageByKey("Ștergere")} (
+              {selectedTickets.length})
             </button>
           )}
 
           {selectedTickets.length > 0 && (
-            <button onClick={() => editSelectedTickets()} className="button-edit-row">
-              <FaEdit /> {getLanguageByKey("Editare")} ({selectedTickets.length})
+            <button
+              onClick={() => editSelectedTickets()}
+              className="button-edit-row"
+            >
+              <FaEdit /> {getLanguageByKey("Editare")} ({selectedTickets.length}
+              )
             </button>
           )}
 
-          <button onClick={() => setIsFilterOpen(true)} className="button-filter">
+          <button
+            onClick={() => setIsFilterOpen(true)}
+            className="button-filter"
+          >
             <FaFilter />
-            {Object.values(filters).some(value => Array.isArray(value) ? value.length > 0 : value) && <span className="filter-indicator"></span>}
+            {Object.values(filters).some((value) =>
+              Array.isArray(value) ? value.length > 0 : value,
+            ) && <span className="filter-indicator"></span>}
           </button>
-
         </div>
       </div>
 
-      <div className="container-tickets">
-        {isTableView ? (
-          <table className="ticket-table">
-            <thead>
-              <tr>
-                <th>{getLanguageByKey("Verificare")}</th>
-                <th>ID</th>
-                <th>{getLanguageByKey("Contact")}</th>
-                <th>{getLanguageByKey("Nume")}</th>
-                <th>{getLanguageByKey("Prenume")}</th>
-                <th>{getLanguageByKey("Email")}</th>
-                <th>{getLanguageByKey("Telefon")}</th>
-                <th>{getLanguageByKey("Descriere")}</th>
-                <th>{getLanguageByKey("Tag-uri")}</th>
-                <th>{getLanguageByKey("Prioritate")}</th>
-                <th>{getLanguageByKey("Workflow")}</th>
-              </tr>
-            </thead>
-            <tbody>
-              {filteredTickets.map((ticket) => (
-                <TicketRow
-                key={ticket.id}
-                ticket={ticket}
-                isSelected={selectedTickets.includes(ticket.id)}
-                onSelect={isTableView ? toggleSelectTicket : undefined} // Только в таблице
-                onEditTicket={setCurrentTicket}
+      <div
+        style={{
+          "--leads-filter-height": `${leadsFilterHeight}px`,
+        }}
+        className="dashboard-container"
+      >
+        <div className="container-tickets">
+          {isTableView ? (
+            <table className="ticket-table">
+              <thead>
+                <tr>
+                  <th>{getLanguageByKey("Verificare")}</th>
+                  <th>ID</th>
+                  <th>{getLanguageByKey("Contact")}</th>
+                  <th>{getLanguageByKey("Nume")}</th>
+                  <th>{getLanguageByKey("Prenume")}</th>
+                  <th>{getLanguageByKey("Email")}</th>
+                  <th>{getLanguageByKey("Telefon")}</th>
+                  <th>{getLanguageByKey("Descriere")}</th>
+                  <th>{getLanguageByKey("Tag-uri")}</th>
+                  <th>{getLanguageByKey("Prioritate")}</th>
+                  <th>{getLanguageByKey("Workflow")}</th>
+                </tr>
+              </thead>
+              <tbody>
+                {filteredTickets.map((ticket) => (
+                  <TicketRow
+                    key={ticket.id}
+                    ticket={ticket}
+                    isSelected={selectedTickets.includes(ticket.id)}
+                    onSelect={isTableView ? toggleSelectTicket : undefined} // Только в таблице
+                    onEditTicket={setCurrentTicket}
+                  />
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            workflowOptions
+              .filter((workflow) => selectedWorkflow.includes(workflow))
+              .map((workflow) => (
+                <WorkflowColumn
+                  key={workflow}
+                  workflow={workflow}
+                  tickets={filteredTickets}
+                  searchTerm={searchTerm}
+                  onEditTicket={(ticket) => {
+                    setCurrentTicket(ticket);
+                    setIsModalOpen(true);
+                  }}
                 />
-              ))}
-            </tbody>
-          </table>
-        ) : (
-          workflowOptions
-            .filter(workflow => selectedWorkflow.includes(workflow))
-            .map((workflow) => (
-              <WorkflowColumn
-                key={workflow}
-                workflow={workflow}
-                tickets={filteredTickets}
-                searchTerm={searchTerm}
-                onEditTicket={(ticket) => {
-                  setCurrentTicket(ticket);
-                  setIsModalOpen(true);
-                }}
-                />
-            ))
+              ))
+          )}
+        </div>
+
+        {isLoading && <SpinnerRightBottom />}
+        {isModalOpen && currentTicket && (
+          <TicketModal
+            ticket={currentTicket}
+            onClose={closeModal}
+            onSave={(updatedTicket) => {
+              setTickets((prevTickets) => {
+                const isEditing = Boolean(updatedTicket.ticket_id);
+                return isEditing
+                  ? prevTickets.map((ticket) =>
+                      ticket.id === updatedTicket.ticket_id
+                        ? updatedTicket
+                        : ticket,
+                    )
+                  : [...prevTickets, updatedTicket];
+              });
+            }}
+          />
         )}
-      </div>
-      {isLoading && <SpinnerRightBottom />}
-      {isModalOpen && currentTicket && (
-        <TicketModal
-          ticket={currentTicket}
-          onClose={closeModal}
-          onSave={(updatedTicket) => {
-            setTickets((prevTickets) => {
-              const isEditing = Boolean(updatedTicket.ticket_id);
-              return isEditing
-                ? prevTickets.map((ticket) =>
-                  ticket.id === updatedTicket.ticket_id ? updatedTicket : ticket
-                )
-                : [...prevTickets, updatedTicket];
+
+        {/* Модальное окно фильтра */}
+        <TicketFilterModal
+          isOpen={isFilterOpen}
+          onClose={() => setIsFilterOpen(false)}
+          filteredTicketIds={filteredTicketIds} // 🔥 Передаем текущие `filteredTicketIds`
+          onApplyFilter={(updatedFilters, ticketIds) => {
+            console.log("🚀 Применяем фильтр с параметрами:", updatedFilters);
+
+            setFilters({
+              ...updatedFilters,
+              technician_id: updatedFilters.technician_id
+                ? updatedFilters.technician_id.map((t) =>
+                    parseInt(t.split(":")[0]),
+                  )
+                : [],
+              priority: updatedFilters.priority || [],
+              platform: updatedFilters.platform || [],
             });
+
+            setSelectedWorkflow(
+              Array.isArray(updatedFilters.workflow)
+                ? updatedFilters.workflow
+                : [],
+            );
+
+            setFilteredTicketIds(ticketIds !== null ? ticketIds : null);
           }}
         />
-      )}
-
-      {/* Модальное окно фильтра */}
-      <TicketFilterModal
-        isOpen={isFilterOpen}
-        onClose={() => setIsFilterOpen(false)}
-        filteredTicketIds={filteredTicketIds} // 🔥 Передаем текущие `filteredTicketIds`
-        onApplyFilter={(updatedFilters, ticketIds) => {
-          console.log("🚀 Применяем фильтр с параметрами:", updatedFilters);
-
-          setFilters({
-            ...updatedFilters,
-            technician_id: updatedFilters.technician_id
-              ? updatedFilters.technician_id.map(t => parseInt(t.split(":")[0]))
-              : [],
-            priority: updatedFilters.priority || [],
-            platform: updatedFilters.platform || [],
-          });
-
-          setSelectedWorkflow(Array.isArray(updatedFilters.workflow) ? updatedFilters.workflow : []);
-
-          setFilteredTicketIds(ticketIds !== null ? ticketIds : null);
-        }}
-      />
-    </div>
+      </div>
+    </>
   );
 };
 
