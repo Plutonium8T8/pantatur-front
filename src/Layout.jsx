bdbf8a4d9108e1605dbb65a5bc49dd8521e9a537
maksimbordan <bordan.maxim@gmail.com>
2025-03-14 11:53:19 +0200

    fix task routes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,12 +2,10 @@ import { useState } from "react"
 import CustomSidebar from "./Components/SideBar/SideBar"
 import UserPage from "./Components/UserPage/UserPage"
 import NotificationModal from "./Components/SlideInComponent/NotificationModal"
-import TaskComponent from "./Components/TaskComponent/TaskPage"
 import { useUser } from "./UserContext"
 import Cookies from "js-cookie"
 
 export const Layout = ({ children }) => {
-  const [isTaskComponentOpen, setIsTaskComponentOpen] = useState(false)
   const [isNotificationModalOpen, setIsNotificationModalOpen] = useState(false)
   const [isAccountComponentOpen, setIsAccountComponentOpen] = useState(false)
 
@@ -24,7 +22,6 @@ export const Layout = ({ children }) => {
     <div className="app-container">
       <CustomSidebar
         onOpenNotifications={() => setIsNotificationModalOpen(true)}
-        onOpenTasks={() => setIsTaskComponentOpen(true)}
         onOpenAccount={() => setIsAccountComponentOpen(true)}
         onLogout={handleLogout}
         userRoles={userRoles}
@@ -37,10 +34,6 @@ export const Layout = ({ children }) => {
       <NotificationModal
         isOpen={isNotificationModalOpen}
         onClose={() => setIsNotificationModalOpen(false)}
-      />
-      <TaskComponent
-        isOpen={isTaskComponentOpen}
-        onClose={() => setIsTaskComponentOpen(false)}
       />
     </div>
   )
